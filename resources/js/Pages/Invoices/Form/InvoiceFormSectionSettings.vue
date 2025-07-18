401b5517463bccd7bc9893a0c751c575e62e1341
Peter Stovka <stovka.peter@gmail.com>
2025-07-18 11:23:51 +0200

    wip

--- resources/js/Pages/Invoices/Form/InvoiceFormSectionSettings.vue
+++ resources/js/Pages/Invoices/Form/InvoiceFormSectionSettings.vue
@@ -88,11 +88,15 @@
 
     <div class="grid grid-cols-2 gap-4">
       <FormControl label="Logo">
-        <img class="h-20 object-contain object-center" v-if="invoice.logoUrl" :src="invoice.logoUrl" alt="">
+        <div class="border border-dashed rounded-md border-input flex items-center justify-center p-2">
+          <img class="h-20 object-contain object-center" v-if="invoice.logoUrl" :src="invoice.logoUrl" alt="">
+        </div>
       </FormControl>
 
       <FormControl label="Podpis">
-        <img class="h-20 object-contain object-center" v-if="invoice.signatureUrl" :src="invoice.signatureUrl" alt="">
+        <div class="border border-dashed rounded-md border-input flex items-center justify-center p-2">
+          <img class="h-20 object-contain object-center" v-if="invoice.signatureUrl" :src="invoice.signatureUrl" alt="">
+        </div>
       </FormControl>
     </div>
   </div>
